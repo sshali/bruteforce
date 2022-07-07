8bca19f9faa39d276af3489fc93288b5a2dc3e49
sshali <106775912+sshali@users.noreply.github.com>
2022-07-07 19:09:05 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,17 @@
+import random
+import pyautogui
+
+character = "0123456789abcdefghijklmnopqrstuvwxyz"
+character_list = list(character)
+
+# 
+password = pyautogui.password('Şifreni Buraya Gir: ')
+
+guess_password = ''  # 
+
+while guess_password != password:
+    guess_password = random.choices(character_list, k=len(password))
+    print('>>>' + str(guess_password) + '<<<<')
+    if guess_password == list(password):
+        pyautogui.confirm('Senin Şifren: ' + ''.join(guess_password))
+        break
